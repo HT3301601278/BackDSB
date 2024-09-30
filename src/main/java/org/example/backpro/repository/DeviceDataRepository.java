6b6547ba85d288b8ab8a35636552334ae2040f0c
世间微斯人 <93172457+HT3301601278@users.noreply.github.com>
2024-09-30 16:48:28 +0800

    更新 DeviceDataRepository.java

--- src/main/java/org/example/backpro/repository/DeviceDataRepository.java
+++ src/main/java/org/example/backpro/repository/DeviceDataRepository.java
@@ -4,7 +4,6 @@ import org.example.backpro.entity.Device;
 import org.example.backpro.entity.DeviceData;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
